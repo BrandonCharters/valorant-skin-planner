85962c4d26114fa3f390a2cad0cb35702b6f9d52
BrandonCharters <101448853+BrandonCharters@users.noreply.github.com>
2024-11-15 16:05:54 +0200

    Created new skin filter

---
+++ src/components/SkinListFilter.jsx
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types';
+import '../Styles/SkinListFilter.css';
+
+export default function SkinListFilter({ onSort, defaultSort }) {
+	// Set the default value of the dropdown to `defaultSort`
+	const handleSortChange = (e) => {
+		const sortOption = e.target.value;
+		onSort(sortOption); // Pass the sort option back to the parent
+	};
+
+	return (
+		<div className="skin-list-filter">
+			<div className="filter-group">
+				<label htmlFor="sort">Sort by: </label>
+				<select
+					id="sort"
+					onChange={handleSortChange}
+					defaultValue={defaultSort}
+				>
+					<option value="lowest">Lowest Price</option>
+					<option value="highest">Highest Price</option>
+				</select>
+			</div>
+		</div>
+	);
+}
+
+SkinListFilter.propTypes = {
+	onSort: PropTypes.func.isRequired, // Required: Function to handle sort logic
+	defaultSort: PropTypes.string, // Optional: Default sort option
+};
+
+SkinListFilter.defaultProps = {
+	defaultSort: 'lowest', // Default to 'lowest' if no default is provided
+};
